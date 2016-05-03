008a175114c28dcb9fd75ef694ff01aab77c221b
Lex Xiong <lex.xiong@boying.com>
2016-05-03 16:31:26 +0800

    日志和路径处理

---
+++ DevSolution/DevSolution.Framework/FileSystems/VirtualPath/IVirtualPathMonitor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevSolution.Caching;
+
+namespace DevSolution.FileSystems.VirtualPath
+{
+    /// <summary>
+    /// Enable monitoring changes over virtual path
+    /// </summary>
+    public interface IVirtualPathMonitor : IVolatileProvider
+    {
+        IVolatileToken WhenPathChanges(string virtualPath);
+    }
+}
